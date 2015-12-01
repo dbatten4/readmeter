9e3d973972e34865dbe7c42cb9c8259db546472b
Dom Batten <dominic.batten@googlemail.com>
2015-12-01 10:48:54 +0000

    opacity parallax added to title box

--- js/appjquery.js
+++ js/appjquery.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
 
+  var titleTarget = $('.title_box');
+  var titleTargetHeight = 200;
+
+  $(document).scroll(function(e) {
+    var scrollPercent = (titleTargetHeight - window.scrollY) / titleTargetHeight;
+    if(scrollPercent >= 0) {
+      titleTarget.css('opacity', scrollPercent);
+    };
+  });
+
+});
