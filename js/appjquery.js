633fe313abf84dcbdf032b39154b74a380788346
Dom Batten <dominic.batten@googlemail.com>
2015-12-01 14:26:49 +0000

    restructured index page, added parallax animation to title box, about to add next user button animation

--- js/appjquery.js
+++ js/appjquery.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
 
-  var titleTarget = $('.title_box');
-  var titleTargetHeight = 200;
+  var titleTarget = $(".title_container");
+  var resultsTarget = $(".results_container");
+  var targetHeight = 200;
 
   $(document).scroll(function(e) {
-    var scrollPercent = (titleTargetHeight - window.scrollY) / titleTargetHeight;
-    if(scrollPercent >= 0) {
-      titleTarget.css('opacity', scrollPercent);
+    var titleScrollPercent = (targetHeight - window.scrollY) / targetHeight;
+    var resultsScrollPercent = 1 - ((targetHeight - window.scrollY) / targetHeight);
+    if(titleScrollPercent >= 0) {
+      titleTarget.css('opacity', titleScrollPercent);
+      resultsTarget.css('opacity', resultsScrollPercent);
     };
+  });
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    $(".title").css("transform","translateY(" + (-scroll / 3) + "px)");
   });
 
 });
