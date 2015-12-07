94113082de033ac1f19b0691c0f940f041d59446
Dom Batten <dominic.batten@googlemail.com>
2015-12-07 18:04:50 +0000

    can now host on server with node

---
+++ public/js/secrets.js
@@ -0,0 +1,1 @@
+gitAccessToken = '2e68ba34f3ff9c07551bcf7c1a35cc8a30386ae5'
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var root = __dirname;
+var path = require('path');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+app.use(express.static(root + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+require('./app/routes.js')(app);
+
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('READMEter listening at http:localhost/3000');
+});
